35a795b8347de7cae6106b78a0f633fab8759ca0
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-09-13 14:26:54 -0300

    Create LefSide

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,31 @@
+import { mainModule } from 'process';
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData'
 
 const Profile: React.FC = () => {
-  return <div />;
+  return <Container>
+    <Main>
+      <LeftSide>
+        <ProfileData
+          username={'VanessaSwerts'}
+          name={'Vanessa Swerts'}
+          avatarURL={'https://avatars2.githubusercontent.com/u/57146734?v=4'}
+          followers={887}
+          following={7}
+          company={'Aurem'}
+          location={'São Paulo, Brazil'}
+          email={'vanessaswerts@gec.inatel.br'}
+          blog={undefined}
+        />
+
+      </LeftSide>
+
+      <RightSide></RightSide>
+    </Main>
+  </Container>
 }
 
 export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    --horinzontalPadding: 16px;
+    --verticalPadding: 24px;
+
+    padding: var(--verticalPadding) var(--horinzontalPadding);
+`;
+
+export const Main = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    margin: 0 auto; 
+    max-width: 1280px;
+
+    @media(min-width: 768px){
+        flex-direction: row; 
+    }
+`;
+
+export const LeftSide = styled.div` 
+  padding: 0 var(--horizontalPadding);
+
+    @media (min-width: 768px){
+        width: 25%; 
+    } 
+`;
+
+export const RightSide = styled.div`
+  padding: 0 var(--horizontalPadding);
+
+  @media (min-width: 768px){
+        width: 75%; 
+    }
+
+`;
