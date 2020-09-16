2dda070bdd6b5b97f580408957ebbed3bffa4fe3
VanessaSwerts <vanessaswerts@gec.inatel.br>
2020-09-15 22:13:20 -0300

    Using API

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,18 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { Container, Main, LeftSide, RightSide, Repos, CalendarHeading, RepoIcon, Tab } from './styles'
 
 import ProfileData from '../../components/ProfileData'
 import RepoCard from '../../components/RepoCard'
 import RandomCalendar from '../../components/RandomCalendar'
+import { APIUser, APIRepo } from '../../@types'
+
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
 
 
 const Profile: React.FC = () => {
+  const { username = 'VanessaSwerts' } = useParams()
+  const [data, setData] = useState<Data>()
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async responses => {
+      const [userResponse, reposResponse] = responses
+
+      if (userResponse.status === 404) {
+        setData({ error: 'User not found!' })
+        return
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.50 - Math.random())
+      const slicedRepos = shuffledRepos.slice(0, 6);
+
+      setData({ user, repos: slicedRepos })
+
+    })
+  }, [username])
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>
+  }
+
+  if (!data?.user || !data?.repos) {
+    return <h1>Loading...</h1>
+  }
 
   const TabContent = () => (
     <div className="content">
       <RepoIcon />
       <span className="label">Repositories</span>
-      <span className="number">15</span>
+      <span className="number">{data.user?.public_repos}</span>
     </div>
   )
 
@@ -28,15 +69,15 @@ const Profile: React.FC = () => {
     <Main>
       <LeftSide>
         <ProfileData
-          username={'VanessaSwerts'}
-          name={'Vanessa Swerts'}
-          avatarURL={'https://avatars2.githubusercontent.com/u/57146734?v=4'}
-          followers={887}
-          following={7}
-          company={'Aurem'}
-          location={'São Paulo, Brazil'}
-          email={'vanessaswerts@gec.inatel.br'}
-          blog={undefined}
+          username={data.user.login}
+          name={data.user.name}
+          avatarURL={data.user.avatar_url}
+          followers={data.user.followers}
+          following={data.user.following}
+          company={data.user.company}
+          location={data.user.location}
+          email={data.user.email}
+          blog={data.user.blog}
         />
       </LeftSide>
 
@@ -49,15 +90,15 @@ const Profile: React.FC = () => {
           <h2>Random Repos</h2>
 
           <div>
-            {[1, 2, 3, 4, 5, 6].map(n => (
+            {data.repos.map((item) => (
               <RepoCard
-                key={n}
-                username={'VanessaSwerts'}
-                reponame={'github-clone'}
-                description={'Clone UI from GitHub'}
-                language={n % 3 === 0 ? 'JavaScript' : 'TypeScript'}
-                stars={8}
-                forks={4}
+                key={item.name}
+                username={item.owner.login}
+                reponame={item.name}
+                description={item.description}
+                language={item.language}
+                stars={item.stargazers_count}
+                forks={item.forks}
               />
             ))}
           </div>
